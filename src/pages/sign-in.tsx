33293c25be11609e8cd646fc6b8368668dedf7dc
Mykolas Viningas <m.viningas@gmail.com>
2023-01-27 00:45:05 +0000

    add form submit for sign-in Co-authored-by: Yusuf Birader <yusuf.birader@hotmail.com> Co-authored-by: Derek Bruinooge <derek.bruinooge@gmail.com> Co-authored-by: Diego Ramirez <dhernandez2448@gmail.com>

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,8 +1,11 @@
 import FormTemplate from '@/components/FormTemplate';
 import { FormikValues } from 'formik';
+import { useRouter } from 'next/router';
 
 const SignIn = () => {
-  const validateUsername = (value: string) => {
+  const router = useRouter();
+
+  const validateUsername = (value: string): string | undefined => {
     let error;
     if (!value) {
       error = 'Username is required';
@@ -11,7 +14,7 @@ const SignIn = () => {
     return error;
   }
 
-  const validatePassword = (value: string) => {
+  const validatePassword = (value: string): string | undefined => {
     let error;
     if (!value) {
       error = 'Password is required';
@@ -20,10 +23,32 @@ const SignIn = () => {
     return error;
   }
 
-  const handleFormSubmit = (values: FormikValues, actions: FormikValues) => {
+  const handleFormSubmit = (values: FormikValues, actions: FormikValues): void => {
     actions.setSubmitting(false);
-    console.log(values);
-    console.log('submitted!');
+
+    let body = JSON.stringify({ username: values.username, passwordHash: values.password });
+    fetch('http://localhost:3001/api/users/login',
+      { method: 'POST', headers: { 'content-type': 'application/json' }, body })
+      .then(response => {
+        return response.json();
+      }).then(data => {
+        let error = data.error;
+        if (error) {
+          let lowercasedError = error.toLowerCase();
+
+          if (lowercasedError.includes('username')) {
+            return actions.setFieldError('username', error);
+          } else if (lowercasedError.includes('password')) {
+            return actions.setFieldError('password', error);
+          } else {
+            return;
+          }
+        }
+
+        router.push(`/dashboard/${data.id}`);
+      }).catch(error => {
+        console.log(error);
+      });
   }
 
   return (
